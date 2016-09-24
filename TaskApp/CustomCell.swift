21713ec7ccb1f6090f87cfb45a5d67fdfb0e9ed9
KOTARO SUTO <ng1a5ss43@gmail.com>
2016-09-25 02:55:00 +0900

    Added custom cells

---
+++ TaskApp/CustomCell.swift
@@ -0,0 +1,31 @@
+//
+//  CustomCell.swift
+//  TaskApp
+//
+//  Created by Kotaro Suto on 2016/09/24.
+//  Copyright © 2016年 Kotaro Suto. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+    
+    @IBOutlet var customBackgroundView: UIView?
+    @IBOutlet var customTitleLabel: UILabel?
+    @IBOutlet var customDateLabel: UILabel?
+    @IBOutlet var customDetailTextView: UITextView?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        customBackgroundView?.layer.cornerRadius = 10.0
+        customBackgroundView?.layer.masksToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TaskApp/RootViewController.swift
@@ -0,0 +1,51 @@
+//
+//  RootViewController.swift
+//  TaskApp
+//
+//  Created by Kotaro Suto on 2016/09/24.
+//  Copyright © 2016年 Kotaro Suto. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    @IBOutlet var table:UITableView!
+    var customCell: CustomCell!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.table.delegate = self
+        self.table.dataSource = self
+        self.table.rowHeight = 293
+        //self.table.rowHeight = UITableViewAutomaticDimension
+        let nib = UINib(nibName: "CustomCell", bundle: nil)
+        table.register(nib, forCellReuseIdentifier: "Cell")
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: - Table View Controller
+    //Table Viewのセルの数を指定
+    func tableView(_ table: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    //各セルの要素を設定する
+    func tableView(_ table: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // tableCell の ID で UITableViewCell のインスタンスを生成
+        let cell = table.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomCell
+        
+        return cell
+    }
+
+}
